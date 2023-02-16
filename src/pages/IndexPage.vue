c81fef6f8a2f7621bfda48ea18d7b0c1ab4df885
divyasonara7 <divyasonara7105@gmail.com>
2023-02-16 16:12:18 +0530

    feat: genrate production build

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,10 +1,6 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+  <h3 class="text-center">Hello, From simform!</h3>
   </q-page>
 </template>
 
