3253ccedaa03c9a8fc67d0cfdee06f4214409dc7
divyasonara7 <divyasonara7105@gmail.com>
2023-02-17 06:10:07 +0530

    feat: add capgo configuration

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,31 @@
 
 <script>
 import { defineComponent } from 'vue'
+import { CapacitorUpdater } from '@capgo/capacitor-updater';
+import { SplashScreen } from '@capacitor/splash-screen';
+import { App } from '@capacitor/app';
+let data = { version: "" };
+CapacitorUpdater.notifyAppReady();
 
+App.addListener('appStateChange', async (state) => {
+  if (state.isActive) {
+    // Do the download during user active app time to prevent failed download
+    data = await CapacitorUpdater.download({
+      version: '1.0.0',
+      url: 'https://github.com/divyasonara7/Capgo/releases/tag/v1.0.0/dist.zip',
+    })
+  }
+  if (!state.isActive && data.version !== "") {
+    // Do the switch when user leave app
+    SplashScreen.show()
+    try {
+      await CapacitorUpdater.set(data)
+    } catch (err) {
+      console.log(err)
+      SplashScreen.hide() // in case the set fail, otherwise the new app will have to hide it
+    }
+  }
+})
 export default defineComponent({
   name: 'App'
 })
